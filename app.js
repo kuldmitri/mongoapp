3d03b428ae450c3976e637d82002f412006d1b30
kuldmitri <KulDmitriBy@gmail.com>
2017-12-12 12:21:51 +0300

    The methods of the book are put in the module services/book.js

--- app.js
+++ app.js
@@ -1,106 +1,17 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-var mongoClient = require("mongodb").MongoClient;
-var objectId = require("mongodb").ObjectID;
+var book = require('./services/book.js');
 
 var app = express();
 var jsonParser = bodyParser.json();
-var url = "mongodb://localhost:27017/Library";
 
 app.use(express.static(__dirname + "/public"));
-app.get("/api/books", function (req, res) {
-
-    mongoClient.connect(url, function (err, db) {
-        db.collection("book").find({}).toArray(function (err, books) {
-            res.send(books);
-            db.close();
-        });
-    });
-});
-
-app.post("/api/issueBook", jsonParser, function (req, res) {
-    if (!req.body) return res.sendStatus(400);
-    var id = new objectId(req.body.id);
-    var abonent = req.body.abonent;
-    var date = new Date();
-    date = date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds();
-
-    mongoClient.connect(url, function (err, db) {
-        db.collection("book").findOneAndUpdate({_id: id}, {$set: {issued: date, issuedto: abonent}},
-            {returnOriginal: false}, function (err, result) {
-                if (err) return res.status(400).send();
-                var book = result.value;
-                console.log(result);
-                res.send(book);
-                db.close();
-            });
-    });
-});
-
-
-app.post("/api/returnBook", jsonParser, function (req, res) {
-    if (!req.body) return res.sendStatus(400);
-    var id = new objectId(req.body.id);
-    mongoClient.connect(url, function (err, db) {
-        db.collection("book").findOneAndUpdate({_id: id}, {$set: {issued: null, issuedto: null}},
-            {returnOriginal: false}, function (err, result) {
-                if (err) return res.status(400).send();
-                var book = result.value;
-                console.log(result);
-                res.send(book);
-                db.close();
-            });
-    });
-});
-
-app.post("/api/findBooks", jsonParser, function (req, res) {
-    var name = req.body.name;
-    var author = req.body.author;
-    console.log('findBooks ' + name + ' ' + author);
-    mongoClient.connect(url, function (err, db) {
-        db.collection("book").find({name: name}).toArray(function (err, books) {
-            res.send(books);
-            console.log(books);
-            db.close();
-        });
-    });
-});
-
-app.post("/api/books/", jsonParser, function (req, res) {
-
-    if (!req.body) return res.sendStatus(400);
-    var book = {};
-    console.log(req.body);
-    book.author = req.body.author;
-    book.name = req.body.name;
-    book.issuedto = req.body.abonent;
-    book.issued = req.body.issued;
-
-    mongoClient.connect(url, function (err, db) {
-        db.collection("book").insertOne(book, function (err, result) {
-            if (err) return res.status(400).send();
-            res.send(result.value);
-            db.close();
-        });
-    });
-});
-
-app.post("/api/deleteBook", jsonParser, function (req, res) {
-
-    if (!req.body) return res.sendStatus(400);
-    var id = new objectId(req.body.id);
-
-    mongoClient.connect(url, function (err, db) {
-        db.collection("book").findOneAndDelete({_id: id}, function (err, result) {
-
-            if (err) return res.status(400).send();
-
-            var book = result.value;
-            res.send(book);
-            db.close();
-        });
-    });
-});
+app.get("/api/books", book.getBooks);
+app.post("/api/issueBook", jsonParser, book.issueBook);
+app.post("/api/returnBook", jsonParser, book.returnBook);
+app.post("/api/findBooks", jsonParser, book.findBooks);
+app.post("/api/addBook", jsonParser, book.addBook);
+app.post("/api/deleteBook", jsonParser, book.deleteBook);
 
 app.listen(3000, function () {
     console.log("Сервер ожидает подключения...");
---
+++ services/book.js
@@ -0,0 +1,89 @@
+var mongoClient = require("mongodb").MongoClient;
+var objectId = require("mongodb").ObjectID;
+var url = "mongodb://localhost:27017/Library";
+
+exports.getBooks = function (req, res) {
+    mongoClient.connect(url, function (err, db) {
+        db.collection("book").find({}).toArray(function (err, books) {
+            res.send(books);
+            db.close();
+        });
+    });
+};
+
+exports.issueBook = function (req, res) {
+    if (!req.body) return res.sendStatus(400);
+    var id = new objectId(req.body.id);
+    var abonent = req.body.abonent;
+    var date = new Date();
+    date = date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds();
+
+    mongoClient.connect(url, function (err, db) {
+        db.collection("book").findOneAndUpdate({_id: id}, {$set: {issued: date, issuedto: abonent}},
+            {returnOriginal: false}, function (err, result) {
+                if (err) return res.status(400).send();
+                var book = result.value;
+                console.log(result);
+                res.send(book);
+                db.close();
+            });
+    });
+};
+
+exports.returnBook = function (req, res) {
+    if (!req.body) return res.sendStatus(400);
+    var id = new objectId(req.body.id);
+    mongoClient.connect(url, function (err, db) {
+        db.collection("book").findOneAndUpdate({_id: id}, {$set: {issued: null, issuedto: null}},
+            {returnOriginal: false}, function (err, result) {
+                if (err) return res.status(400).send();
+                var book = result.value;
+                console.log(result);
+                res.send(book);
+                db.close();
+            });
+    });
+};
+
+exports.findBooks = function (req, res) {
+    var name = req.body.name;
+    var author = req.body.author;
+    console.log('findBooks ' + name + ' ' + author);
+    mongoClient.connect(url, function (err, db) {
+        db.collection("book").find({name: name}).toArray(function (err, books) {
+            res.send(books);
+            console.log(books);
+            db.close();
+        });
+    });
+};
+
+exports.addBook = function (req, res) {
+    if (!req.body) return res.sendStatus(400);
+    var book = {};
+    console.log(req.body);
+    book.author = req.body.author;
+    book.name = req.body.name;
+    book.issuedto = req.body.abonent;
+    book.issued = req.body.issued;
+    mongoClient.connect(url, function (err, db) {
+        db.collection("book").insertOne(book, function (err, result) {
+            if (err) return res.status(400).send();
+            res.send(result.value);
+            db.close();
+        });
+    });
+};
+
+exports.deleteBook = function (req, res) {
+    if (!req.body) return res.sendStatus(400);
+    var id = new objectId(req.body.id);
+    mongoClient.connect(url, function (err, db) {
+        db.collection("book").findOneAndDelete({_id: id}, function (err, result) {
+            if (err) return res.status(400).send();
+            var book = result.value;
+            res.send(book);
+            db.close();
+        });
+    });
+};
